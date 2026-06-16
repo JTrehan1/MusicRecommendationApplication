adab6b0912c257229495bac20d8776297ea9ddc0
Jai Trehan <jai.trehan@cgi.com>
2026-06-16 17:58:00 +0100

    Docstring added and model training class ready to be developed.

--- packages/modeling/model_training.py
+++ packages/modeling/model_training.py
@@ -5,14 +5,19 @@ import logging
 from implicit.als import AlternatingLeastSquares
 from implicit.bpr import BayesianPersonalizedRanking
 from implicit.evaluation import precision_at_k, mean_average_precision_at_k 
+from typing import Any
 
 logger = logging.getLogger(__name__)
 
 @dataclass
 class ModelTrainingOutput:
-    trained_model: dict 
+    trained_model: Any
+    model_type: str 
     track_uri_to_index: dict
     index_to_track_uri: dict
     hyperparameters: dict
     evaluation_scores: dict 
-    
+
+class ModelTraining:
+    """Class for containing the model training of both ALS and BPR models. Run using dedicated orchestration script. 
+    """
